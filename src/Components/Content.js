4f21a51a3cc5001dff0644af6bbcddcc5f1c68ee
Ahsan Zia <Ahsan97@hotmail.co.uk>
2020-07-10 19:43:17 +0100

    did work on logging in function

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,29 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { password, username } from './secrets'
+import { ticktick, username, password } from './secrets'
 
 export default class Content extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = { 
-            username: undefined 
+        this.state = {
+            username: undefined
         }
+        
+    }
 
+    async componentWillMount() {
+        this.getTickTickData();
     }
 
     async getTickTickData() {
-        const ticktick = require('ticktick-wrapper');
+
         await ticktick.login({
             email: {
                 username: username,
                 password: password,
             },
         });
+
+        var d = new Date();
+
+        console.log(d.getDay() + "." + d.getMonth)
+
+        ticktick.getListByName("J").then(res => res.getTodoTasks().then(res2 => {
+
+        }))
     }
 
     render() {
         return <div className='message-box'>
+            <button onClick={this.getTickTickData()}> Load Habits</button>
             Hello {this.props.name}
         </div>
     }
